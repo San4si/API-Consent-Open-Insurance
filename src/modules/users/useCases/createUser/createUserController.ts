7fd67cfb1a22fdd15ca9df3573ed86c994b14320
soft <sandro.brito@gft.com>
2022-12-22 11:28:41 -0300

    Fix Create User

--- src/modules/users/useCases/createUser/createUserController.ts
+++ src/modules/users/useCases/createUser/createUserController.ts
@@ -4,16 +4,17 @@ import { CreateUserCase } from "./createUserCase";
 class CreateUserController {
   async handle(req: Request, res: Response) {
     const { name, email, password } = req.body;
-
     const createUserCase = new CreateUserCase();
-
-    const user = await createUserCase.execute({
-      name,
-      email,
-      password,
-    });
-
-    return res.status(201).json(user);
+    try {
+      const user = await createUserCase.execute({
+        name,
+        email,
+        password,
+      });
+      return res.status(201).json(user);
+    } catch ({ message }) {
+      res.status(400).json({ message });
+    }
   }
 }
 
